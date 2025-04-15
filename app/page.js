691fec21ff92b6d4accb210fa094b4f5eab56e6f
Hammad Khan <hammad283khan@gmail.com>
2025-04-15 05:14:56 -0700

    url updated

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
         type: 'SEND_PARENT_ORIGIN',
         origin: window.location.origin, // sending the embedding site's origin
       },
-      'http://localhost:3000' // replace this with your widget's domain
+      'https://webapp-andru-ai-fe-dev-westus2-001.azurewebsites.net' // replace this with your widget's domain
     );
   };
 
@@ -15,7 +15,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] h-screen bg-white items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <iframe
         id="widget-iframe" // Adding an id for reference
-        src="http://localhost:3000/widget/72ea0de1-262f-46a4-8ab3-db015f4cc9ea?callerId=your-subscriber-id"
+        src="https://webapp-andru-ai-fe-dev-westus2-001.azurewebsites.net/widget/72ea0de1-262f-46a4-8ab3-db015f4cc9ea?callerId=your-subscriber-id"
         width="100%"
         height="500px"
         frameborder="0"
